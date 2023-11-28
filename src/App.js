39bd729ad8c931fb7f7eea9aeabb4f21841b4daf
DESKTOP-98JOUDC\user <jojoo9279@gmail.com>
2023-11-28 15:56:07 +0900

    [Crew 찾기 페이지]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Notice from './pages/Notice';
+import Crew from './pages/Crew';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Layout/> }>
           <Route path="notice" element={<Notice/>}/>
+            <Route path="/crew" element={<Crew />} />
         </Route>
       </Routes>
 
