a5ad169f8201a471882f9aea7e98c1ae229d8772
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-11-28 15:43:11 +0900

    연결 테스트 커밋

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Notice from './pages/Notice';
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Layout/> }>
           <Route path="notice" element={<Notice/>}/>
+          <Route path="mypage" element={<MyPage/>}/>
         </Route>
       </Routes>
 
