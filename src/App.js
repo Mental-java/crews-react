5273e501517334936d8914efac5d48ef5fc68772
Hongse0 <hongseyoung981007@gmail.com>
2023-12-02 14:49:40 +0900

    loginhandler 추가, loginlayout추가 (기능은 실행 안됨)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import LoginLayout from './layouts/LoginLayout';
 import Notice from './pages/Notice';
 import MyPage from "./pages/MyPage";
 import CrewSearch from "./pages/CrewSearch";
 import CreateCrew from "./pages/CreateCrew";
+import Login from './pages/login/Login';
+import LoginHandler from './apis/LoginHandler';
 
 function App() {
   return (
    <BrowserRouter>
 
       <Routes>
-        <Route path="/" element={ <Layout/> }>
-          <Route path="notice" element={<Notice/>}/>
-          <Route path="mypage" element={<MyPage/>}/>
-          <Route path="crewsearch" element={<CrewSearch/>}/>
-          <Route path="createcrew" element={<CreateCrew/>}/>
+
+        <Route path="/" element={ <LoginLayout/>}>
+
+          <Route index element={ <Login/>}/>
+          <Route path="callback" element={ <LoginHandler/>}/>
+        
         </Route>
+
+        <Route path="main" element={ <Layout/> }>
+            <Route path="notice" element={<Notice/>}/>
+            <Route path="mypage" element={<MyPage/>}/>
+            <Route path="crewsearch" element={<CrewSearch/>}/>
+            <Route path="createcrew" element={<CreateCrew/>}/>
+        </Route>
+        
       </Routes>
 
    </BrowserRouter>
