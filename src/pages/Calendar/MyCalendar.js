d05ec980d1f50ac1a9c6ed8f5cf1770c61600f1a
DESKTOP-98JOUDC\user <jojoo9279@gmail.com>
2023-12-05 18:18:02 +0900

    [모달창에 제목, 시작일, 종료일 띄우기]

--- src/pages/Calendar/MyCalendar.js
+++ src/pages/Calendar/MyCalendar.js
@@ -7,19 +7,21 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import MyModal from "./MyModal";
 
 function MyCalendar() {
-    const [modalIsOpen, setModalIsOpen] = useState(false); // 모달의 상태를 설정하는 state
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
     const dispatch = useDispatch();
     const mycalendar = useSelector((state) => state.myCalendarReducer);
-    const myCalendarList = mycalendar.data ? mycalendar.data.map((calendar) => ({
-        end: calendar.endDate,
-        start: calendar.startDate,
-        title: calendar.title
-    })) : [];
+    const myCalendarList = mycalendar.data;
     console.log(myCalendarList)
 
     useEffect(() => {
         dispatch(callMyCalendarListAPI());
     }, []);
+
+    const handleEventClick = (info) => {
+        setModalIsOpen(true);
+        setSelectedEvent(info.event);
+    };
 
     return (
         <div className={MyCalendarCSS.MyCalendarContainer}>
@@ -27,17 +29,25 @@ function MyCalendar() {
                 firstDay={1}
                 initialView="dayGridMonth"
                 plugins={[dayGridPlugin]}
-                events={myCalendarList}
+                events={Array.isArray(myCalendarList)
+                    ? myCalendarList.map((calendar) => ({
+                        end: calendar.endDate,
+                        start: calendar.startDate,
+                        title: calendar.title,
+                    }))
+                    : []
+                }
                 height={'95vh'}
                 editable={true}
-                eventClick={function(info) { // 이벤트 클릭 시 모달을 띄우기 위한 함수
-                    setModalIsOpen(true);
-                }}
+                eventClick={handleEventClick}
             />
-            <MyModal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setModalIsOpen(false)}
-            />
+            {modalIsOpen && (
+                <MyModal
+                    isOpen={modalIsOpen}
+                    onRequestClose={() => setModalIsOpen(false)}
+                    event={selectedEvent}
+                />
+            )}
         </div>
     );
 }
