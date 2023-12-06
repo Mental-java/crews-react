0cfaae6c2b86611c23ea0ec427b11fef03a27618
Hongse0 <hongseyoung981007@gmail.com>
2023-12-06 18:45:37 +0900

    닉네임 변경 모달창 커밋

--- src/apis/MyPageAPICalls.js
+++ src/apis/MyPageAPICalls.js
@@ -1,4 +1,4 @@
-import { GET_MYPAGE } from "../module/MyPageModule";
+import { GET_MYPAGE, PUT_NICKNAME } from "../module/MyPageModule";
 
 
 export const callMyPageListAPI = () => {
@@ -19,4 +19,28 @@ export const callMyPageListAPI = () => {
         console.log('[callMyPageList test] Result =======> ', result);
         dispatch({ type: GET_MYPAGE, payload: result.data });
     };
+}
+
+export const callNickNameChangeAPI = ({form}) => {
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8080/api/v1/user/editnickname`
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "*/*",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+                "userId": "form.userId",
+                "nickname": "form.nickname"
+            })
+        })
+        .then(response => response.json());
+
+        dispatch({ type:PUT_NICKNAME, payload: result.data});
+    };
+
 }
