600e8f3b340b3328cf1e760f79d3b497c84601cf
zusung47 <zusung47@gmail.com>
2023-12-08 01:05:08 +0900

    새로고침 정상작동

--- src/Store.js
+++ src/Store.js
@@ -1,11 +1,34 @@
-import rootReducer from './module';
+// import rootReducer from './module';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import ReduxThunk from 'redux-thunk';
+//
+// const store = createStore(
+//     rootReducer,
+//     composeWithDevTools(applyMiddleware(ReduxThunk))
+// );
+//
+// export default store;
+
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+import rootReducer from './module';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(ReduxThunk))
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
+export const persistor = persistStore(store);
 export default store;
