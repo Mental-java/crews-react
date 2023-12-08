bc48599d5a30abd7669a5c0661e2cd7f769d3deb
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2023-12-08 14:45:40 +0900

    크루페이지 네비게이션 수정

--- src/pages/crew/CrewCertification.js
+++ src/pages/crew/CrewCertification.js
@@ -1,6 +1,7 @@
 import {NavLink} from "react-router-dom";
 import React from "react";
 import CrewCSS from "./CrewCommon.module.css";
+import CertificationCSS from "./CrewCertification.module.css";
 
 function CrewCertification () {
     return(
@@ -8,9 +9,9 @@ function CrewCertification () {
         <div>
             <div>
                 <ul>
-                    <li><NavLink to="/main/crewCalendar" className={CrewCSS.mainLink}>크루 메인 페이지</NavLink></li>
-                    <li><NavLink to="/main/crewCertification" className={CrewCSS.mainLink}>인증게시판</NavLink></li>
-                    <li><NavLink to="/main/activeStatus" className={CrewCSS.mainLink}>활동현황</NavLink></li>
+                    <li><NavLink to="/main/crewMain" className={CrewCSS.crewPage}>크루 메인 페이지</NavLink></li>
+                    <li><NavLink to="/main/crewCertification" className={`${CrewCSS.crewPage} ${CertificationCSS.certification}`}>인증게시판</NavLink></li>
+                    <li><NavLink to="/main/activeStatus" className={CrewCSS.crewPage}>활동현황</NavLink></li>
                 </ul>
             </div>
             <hr/>
--- src/pages/crew/CrewMain.js
+++ src/pages/crew/CrewMain.js
@@ -3,8 +3,9 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {NavLink} from "react-router-dom";
 import CrewCSS from "./CrewCommon.module.css";
+import CrewMainCSS from "./CrewMain.module.css";
 
-function CrewCalendar() {
+function CrewMain() {
     // const crewcalendar = useSelector((state) => state.myCalendarReducer);
     // const crewCalendarList = crewcalendar.data;
     //
@@ -18,9 +19,9 @@ function CrewCalendar() {
         <div>
             <div>
                 <ul>
-                    <li><NavLink to="/main/crewCalendar" className={CrewCSS.mainLink}>크루 메인 페이지</NavLink></li>
-                    <li><NavLink to="/main/crewCertification" className={CrewCSS.mainLink}>인증게시판</NavLink></li>
-                    <li><NavLink to="/main/activeStatus" className={CrewCSS.mainLink}>활동현황</NavLink></li>
+                    <li><NavLink to="/main/crewMain" className={`${CrewCSS.crewPage} ${CrewMainCSS.main}`}>크루 메인 페이지</NavLink></li>
+                    <li><NavLink to="/main/crewCertification" className={CrewCSS.crewPage}>인증게시판</NavLink></li>
+                    <li><NavLink to="/main/activeStatus" className={CrewCSS.crewPage}>활동현황</NavLink></li>
                 </ul>
             </div>
             <hr/>
@@ -38,4 +39,4 @@ function CrewCalendar() {
     );
 }
 
-export default CrewCalendar;
+export default CrewMain;
