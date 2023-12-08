330754647465cb6f1e8f57379b4774d55dae57ed
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2023-12-08 18:04:43 +0900

    게시글 작성

--- src/pages/crew/WritePost.js
+++ src/pages/crew/WritePost.js
@@ -1,8 +1,25 @@
+import PostCSS from "./WritePost.module.css";
+
 function writePost() {
 
     return(
         <div>
-            게시글 작성
+            <div>
+                <p className={PostCSS.post}>게시글 작성</p>
+            </div>
+            <hr/>
+            <div className="post-title">
+                <p>제목</p>
+                <input type="text" name="postTitle" required/>
+            </div>
+            <div className="post-content">
+                <p>내용</p>
+                <textarea required rows="15" cols="100"></textarea>
+            </div>
+            <div className={PostCSS.button}>
+                등록
+            </div>
+
         </div>
     );
 
