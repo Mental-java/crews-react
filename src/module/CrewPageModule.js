e4b6ee32a916571d742ac1ac43b93f00bc3c5753
Hongse0 <hongseyoung981007@gmail.com>
2023-12-16 01:29:31 +0900

    오류 수정

--- src/module/CrewPageModule.js
+++ src/module/CrewPageModule.js
@@ -4,15 +4,20 @@ const initialState = [];
 
 export const GET_CREWPOST = 'crewPage/GET_CREWPOST';
 
-const actions = createActions({
-    [GET_CREWPOST]: () => { }
-});
+export const getCrewPost = () => ({
+    type: GET_CREWPOST,
+  });
+  
+  const actions = createActions({
+      [GET_CREWPOST]: getCrewPost, // 액션 생성자 함수 사용
+  });
+  
 
 
 const crewPageReducer = handleActions(
     {
         [GET_CREWPOST]: (state, { payload }) => {
-            return payload;
+            return state;
         }
     },
     initialState
