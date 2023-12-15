459ba9489883c0aac9069062efd3a335ab1a4101
Hongse0 <hongseyoung981007@gmail.com>
2023-12-15 19:13:06 +0900

    관리자 로그인 화면 제작, 로그인 후 관리자 페이지에서 로그아웃할수 있게 제작

--- src/apis/AdminAPICalls.js
+++ src/apis/AdminAPICalls.js
@@ -4,7 +4,8 @@ import {
     ADMIN_USERLIST,
     ADMIN_CREWLIST,
     ADMIN_USERREPORTLIST,
-    ADMIN_CREWREPORTLIST
+    ADMIN_CREWREPORTLIST,
+    POST_ADMINLOGIN
 } from "../module/AdminModule"
 
 // export const callNoticeListAPI = ({noticeId}) => {
@@ -51,3 +52,44 @@ export const callAdminCrewListAPI = ({currentPage}) => {
 // export const callCrewReportListAPI = ({crewReportId}) => {
 //
 // }
+
+export const callAdminLoginAPI = ({form}) => {
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8080/admin/login`;
+
+    return async (dispatch, getState) => {
+
+        // 클라이언트 fetch mode : no-cors 사용시 application/json 방식으로 요청이 불가능
+        // 보안상의 이유로 브라우저는 스크립트에서 시작한 교차 출처 HTTP요청을 제한한다.
+        // 서버에서 cors 허용을 해주어야 함
+        const result = await fetch(requestURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "*/*",
+                "Access-Control-Allow-Origin": "*"     // 모든 도멘인에서 접근할 수 있음을 의미 (특정도메인을 넣고싶으면 * 대신 http://test.com)
+            },
+            body: JSON.stringify({
+                adminId: form.adminId,
+                adminPw: form.adminPassword
+            })
+        })
+        .then(response => response.json());
+
+        console.log('[MemberAPICalls] callLoginAPI RESULT : ', result);
+        if(result.status === 200){
+            window.localStorage.setItem('adminAccessToken', result.data.accessToken);            
+        }
+        dispatch({ type: POST_ADMINLOGIN,  payload: result });
+        
+    };
+}
+
+export const callLogoutAPI = () => {
+    
+
+    return async (dispatch, getState) => {            
+
+        dispatch({ type: POST_ADMINLOGIN,  payload: '' });        
+        console.log('[MemberAPICalls] callLogoutAPI RESULT : SUCCESS');
+    };
+}
