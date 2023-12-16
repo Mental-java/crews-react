2c5c6ad6690008c91d4d9520d7d4e0ba9e22a8b8
zusung47 <zusung47@gmail.com>
2023-12-16 19:01:06 +0900

    다이아별 사진수정/크루날짜별체크

--- src/module/CrewCheckModule.js
+++ src/module/CrewCheckModule.js
@@ -3,14 +3,20 @@ import {createActions, handleActions} from "redux-actions";
 const initialState = [];
 
 export const GET_CREWCHECK = 'crewCheck/GET_CREWCHECK';
+export const PUT_CREWCHECK = 'crewCheck/PUT_CREWCHECK';
 
 const actions = createActions({
-    [GET_CREWCHECK]: () => {}
+    [GET_CREWCHECK]: () => {},
+    [PUT_CREWCHECK]: () => {}
 });
 
 const crewCheckReducer = handleActions(
     {
         [GET_CREWCHECK]: (state, {payload}) => {
+
+            return payload;
+        },
+        [PUT_CREWCHECK]: (state, {payload}) => {
 
             return payload;
         }
