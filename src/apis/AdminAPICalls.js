38e914cf1ed95d2499f13b25c6bc97db86333f22
Hongse0 <hongseyoung981007@gmail.com>
2023-12-17 22:12:34 +0900

    수정

--- src/apis/AdminAPICalls.js
+++ src/apis/AdminAPICalls.js
@@ -150,10 +150,8 @@ export const callCrewReportListAPI = ({currentPage}) => {
 
 
             }
-
-
-        })
-            .then(response => response.json());
+        )
+         .then(response => response.json());
 
         console.log('[AdminAPICalls] AdminCrewReportListAPI RESULT : ', result);
         dispatch({type: ADMIN_CREWREPORTLIST, payload: result.data});
