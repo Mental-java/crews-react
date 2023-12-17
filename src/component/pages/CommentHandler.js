c6e3a5eb15ecb5cf1f255d16414a832857e95078
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2023-12-17 23:34:44 +0900

    댓글화면 수정

--- src/component/pages/CommentHandler.js
+++ src/component/pages/CommentHandler.js
@@ -2,27 +2,23 @@ import CertificationDetailCSS from "../../pages/crew/CertificationDetail.module.
 
 function CommentHandler({ commentInfo: { userId, commentContent, writeDate, commentImageUrl}}){
     return(
-        <div className={CertificationDetailCSS.scrollableWrapper}>
-            <table>
-                <tbody>
-                <tr>
-                    <td className={CertificationDetailCSS.tdDiv}>
-                        <div className={CertificationDetailCSS.contentDiv}>
-                            <div className={CertificationDetailCSS.nicknameDiv}>
-                                {userId.nickname}&nbsp;&nbsp;&nbsp;{writeDate}
-                            </div>
-                            <div className={CertificationDetailCSS.commentDiv}>
-                                {commentContent}
-                            </div>
+        <>
+            <tr>
+                <td className={CertificationDetailCSS.tdDiv}>
+                    <div className={CertificationDetailCSS.contentDiv}>
+                        <div className={CertificationDetailCSS.nicknameDiv}>
+                            {userId.nickname}&nbsp;&nbsp;&nbsp;{writeDate}
                         </div>
-                        <div className={CertificationDetailCSS.imageDiv}>
-                            <img src={commentImageUrl} alt="Comment Image"/>
+                        <div className={CertificationDetailCSS.commentDiv}>
+                            {commentContent}
                         </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+                    </div>
+                    <div className={CertificationDetailCSS.imageDiv}>
+                        <img src={commentImageUrl}/>
+                    </div>
+                </td>
+            </tr>
+        </>
     )
 }
 
