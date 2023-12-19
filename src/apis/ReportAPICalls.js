d6d7ba63cd086fc0c2cba2481f4b5eaa159b5f32
zusung47 <zusung47@gmail.com>
2023-12-19 14:32:55 +0900

    유저 신고 구현

--- src/apis/ReportAPICalls.js
+++ src/apis/ReportAPICalls.js
@@ -1,6 +1,7 @@
 
 import {
-    POST_CREW_REPORT
+    POST_CREW_REPORT,
+    POST_USER_REPORT
 } from '../module/ReportModule';
 
 export const callCrewReportWriteAPI = ({form}) => {
@@ -30,4 +31,32 @@ export const callCrewReportWriteAPI = ({form}) => {
         dispatch({ type: POST_CREW_REPORT, payload: result });
     };
 
+}
+
+export const callUserReportWriteAPI = ({form}) => {
+    console.log('[ReportAPICalls] callUserReportWriteAPI call');
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8080/api/v1/report/user`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "*/*"
+            },
+            body: JSON.stringify({
+                reportContent: form.reportContent,
+                reporter: {userId: form.reporter.userId},
+                reportCategory: form.reportCategory,
+                reportTarget: {userId: form.reportTarget}
+            })
+        })
+            .then(response => response.json());
+
+        console.log('[ReportAPICalls] callUserReportWriteAPI result : ', result);
+
+        dispatch({ type: POST_USER_REPORT, payload: result });
+    };
 }
--- src/component/pages/UserInCrewHandler.js
+++ src/component/pages/UserInCrewHandler.js
@@ -5,6 +5,7 @@ import diamondImg from "../../img/diamond-image.png";
 
 import ActiveStatusCSS from "../../pages/crew/ActiveStatus.module.css"
 import CrewCheckModal from "../../pages/crew/CrewCheckModal";
+import UserReportModal from "../../pages/report/UserReportModal";
 
 function UserInCrewHandler({ userInfo: { user, crew, checkCount, isCaptain } }){
 
@@ -15,14 +16,25 @@ function UserInCrewHandler({ userInfo: { user, crew, checkCount, isCaptain } }){
     const [crewCheckModal, setCrewCheckModal] = useState(false);
     const [userEmail, setUserEmail] = useState('');
 
+    const [reportModal, setReportModal] = useState(false);
+    const [reportUserId, setReportUserId] = useState('');
+    const [reportUserName, setReportUserName] = useState('');
+
     const onClickCrewCheckModalHandler = (userId) => {
         setCrewCheckModal(true);
         setUserEmail(userId);
     }
 
+    const onClickReportModalHandler = (userId, userName) => {
+        setReportModal(true);
+        setReportUserId(userId);
+        setReportUserName(userName);
+    }
+
     return(
         <>
             { crewCheckModal ? <CrewCheckModal setCrewCheckModal={setCrewCheckModal} userId={userEmail}/> : null}
+            { reportModal ? <UserReportModal setReportModal={setReportModal} reportTarget={reportUserId} reportTargetName={reportUserName}/> : null}
             <tr className={ActiveStatusCSS.trDiv}>
                 <td className={ActiveStatusCSS.tdDiv}>
                     <div className={ActiveStatusCSS.isCaptainDiv}>
@@ -40,6 +52,12 @@ function UserInCrewHandler({ userInfo: { user, crew, checkCount, isCaptain } }){
                     <div className={ActiveStatusCSS.checkBtnDiv}>
                         <div className={ActiveStatusCSS.checkDiv} onClick={() => onClickCrewCheckModalHandler(user.userId)}>
                             활동 내역
+                        </div>
+                        <div
+                            className={ActiveStatusCSS.reportBtn}
+                            onClick={() => onClickReportModalHandler(user.userId, user.nickname)}
+                        >
+                            신고하기
                         </div>
                     </div>
                 </td>
